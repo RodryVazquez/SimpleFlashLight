38f34f8164e1e7d804cca1a94d813a5e1489ea45
RodryVazquez <rodrigo.vazquez@intelectix.com>
2015-11-30 23:56:42 -0700

    Se agrego permiso para usar la camara dentro del manifiesto. Se agrego logica para manejo del flash,  sobreescribimos los metodos para manejar el ciclo de vida de la aplicacion

--- app/src/main/java/com/example/rodrigo/simpleflashlight/MainActivity.java
+++ app/src/main/java/com/example/rodrigo/simpleflashlight/MainActivity.java
@@ -13,6 +13,7 @@ import android.graphics.Camera;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.method.CharacterPickerDialog;
+import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
@@ -22,7 +23,7 @@ import java.security.Policy;
 //Activty principal
 public class MainActivity extends ActionBarActivity {
 
-    //
+
     private android.hardware.Camera camera;
     private boolean isFlashOn;
     private boolean hasFlash;
@@ -34,12 +35,12 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Obtener la refencia al boton
-        ToggleButton btnFlashButton = (ToggleButton)findViewById(R.id.btnFlash_Switch);
+        ToggleButton btnFlashButton = (ToggleButton) findViewById(R.id.btnFlash_Switch);
 
         //Verificamos que el dispositivo soporte el flash de la camara
         hasFlash = getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
         //Si el dispositivo no soporta el flash, mostramos una alerta y cerramos la aplicacion
-        if(!hasFlash){
+        if (!hasFlash) {
             AlertDialog alert = new AlertDialog.Builder(this).create();
             alert.setTitle("Error");
             alert.setMessage("Your device doesn't support flash light!!");
@@ -56,12 +57,83 @@ public class MainActivity extends ActionBarActivity {
         btnFlashButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-
-                }else{
-
+                if (isChecked) {
+                    turnOnFlash();
+                } else {
+                    turnOffFlash();
                 }
             }
         });
+
+    }
+
+    //Al iniciar el ciclo de vida visible, obtenemos la camara
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getCamera();
+    }
+
+    //Al finalizar el ciclo de vida activo, apagamos el flash
+    @Override
+    protected void onPause() {
+        super.onPause();
+        turnOffFlash();
+    }
+
+    //Al finalizar el ciclo de vida visible, liberamos la camara
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(camera != null){
+            camera.release();
+            camera = null;
+        }
+    }
+
+    //Inicializamos la camara
+    private void getCamera() {
+        if (camera == null) {
+            try {
+                camera = android.hardware.Camera.open();
+                parameters = camera.getParameters();
+            } catch (RuntimeException e) {
+                Log.e("Camera Error. Error: ", e.getMessage());
+            }
+        }
+    }
+
+    //Encendemos el flash de la camara
+    private void turnOnFlash() {
+        //Si el flash se encuentra apagado
+        if (!isFlashOn) {
+            if (camera == null || parameters == null) {
+                return;
+            }
+            parameters = camera.getParameters();
+            parameters.setFlashMode(android.hardware.Camera.Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(parameters);
+            camera.startPreview();
+            isFlashOn = true;
+
+            Log.v("Android ATC", "Flash has been turned on ...");
+        }
+    }
+
+    //Apagamos el flash de la camaras
+    private void turnOffFlash() {
+        //Si el flash se encuentra encendido
+        if (isFlashOn) {
+            if (camera == null || parameters == null) {
+                return;
+            }
+            parameters = camera.getParameters();
+            parameters.setFlashMode(android.hardware.Camera.Parameters.FLASH_MODE_OFF);
+            camera.setParameters(parameters);
+            camera.stopPreview();
+            isFlashOn = false;
+
+            Log.v("Android ATC", "Flash has been turned off ...");
+        }
     }
 }
